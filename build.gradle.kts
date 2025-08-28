0337a9903522d9cd6a3cd4160604f78725eaa264
suiyun39 <suiyun39@gmail.com>
2025-08-29 07:14:00 +0800

    chore: 添加 Spring Web 和 Actuator 依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,8 @@ repositories {
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter")
+  implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("org.springframework.boot:spring-boot-starter-actuator")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
